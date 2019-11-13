2ff4f8773e36242f4506a3302ef969cdee859276
thisisalin <alintroncea@gmail.com>
2019-11-13 15:59:50 +0200

    first updated

---
+++ EchoClient/ClientProgram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient
+{
+    class ClientProgram
+    {
+        static void Main(string[] args)
+        {
+            string serverIP = "localhost";
+            int port = 8080;
+
+            TcpClient client = new TcpClient(serverIP, port);
+
+            while (true)
+            {
+                string msg = Console.ReadLine();
+
+                int byteCount = Encoding.ASCII.GetByteCount(msg);
+
+                byte[] sendData = new byte[byteCount];
+
+                sendData = Encoding.ASCII.GetBytes(msg);
+
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(sendData, 0, sendData.Length);
+
+                stream.Close();
+                client.Close();
+            }
+        }
+    }
+}
---
+++ EchoServer/ServerProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoServer
+{
+    class ServerProgram
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = Dns.GetHostEntry("localhost").AddressList[0];
+            TcpListener server = new TcpListener(ipAddress, 8080);
+            TcpClient client = new TcpClient();
+
+            server.Start();
+            Console.WriteLine("Server started....");
+
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+
+                byte[] receivedBuffer = new byte[100];
+                NetworkStream stream = client.GetStream();
+
+                stream.Read(receivedBuffer, 0, receivedBuffer.Length);
+
+                string msg = Encoding.ASCII.GetString(receivedBuffer, 0, receivedBuffer.Length);
+
+                Console.WriteLine("The client says: " + msg);
+
+            }
+
+        }
+    }
+}
